f333b849a211ff8bb3d5990c98eae852622992c4
tundera <stackshuffle@gmail.com>
2020-07-11 20:34:51 -0600

    remove commented in lin e

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 // This import is only needed when checking authentication status directly from getInitialProps
-import auth0 from '../../lib/auth0';
+// import auth0 from '../../lib/auth0';
 import { NextPage } from 'next';
 
 import Layout from '../components/Layout';
